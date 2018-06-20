9287b9c435543773267ef5c5fcf0f8a1c6179504
Adrian Apodaca <adrian@byvapps.com>
2018-06-20 16:39:37 +0200

    : split if need

--- ByvStylesIB/Classes/ByvDesignableLoader.swift
+++ ByvStylesIB/Classes/ByvDesignableLoader.swift
@@ -13,7 +13,8 @@ class ByvDesignableLoader: NSObject {
         let processInfo = ProcessInfo.processInfo
         let environment = processInfo.environment
         let projectSourceDirectories: String = environment["IB_PROJECT_SOURCE_DIRECTORIES"]!
-        let zeplinPath = projectSourceDirectories.replacingOccurrences(of: "Pods", with: "zeplin")
+        var zeplinPath = projectSourceDirectories.replacingOccurrences(of: "Pods", with: "zeplin")
+        zeplinPath = zeplinPath.components(separatedBy: ":")[0]
         ByvColors.loadColors(path: zeplinPath + "/Colors.json")
         ByvStyles.loadStyles(path: zeplinPath + "/Styles.json")
     }
